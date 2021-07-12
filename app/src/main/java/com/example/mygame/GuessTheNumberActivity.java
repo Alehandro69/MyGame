adcb2e722e9a951aaa53ad8c561472aa5aa87915
wiind <work.personal.filippov@gmail.com>
2021-07-12 19:23:17 +0300

    Test nEw2

--- app/src/main/java/com/example/mygame/GuessTheNumberActivity.java
+++ app/src/main/java/com/example/mygame/GuessTheNumberActivity.java
@@ -4,36 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.Random;
 import java.util.Scanner;
 import android.os.Bundle;
+import android.renderscript.Sampler;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class GuessTheNumberActivity extends AppCompatActivity {
 
-    private TextView titleTv;
+    private EditText enterEt;
+    private ImageView goIv;
+    private TextView endTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guess_the_number);
 
-        titleTv = findViewById(R.id.titleTv);
-        titleTv.setText("Я загадал число от 0 до 1000. /end Ваш ход.");
+        enterEt = findViewById(R.id.enterEt);
+        endTv = findViewById(R.id.endTv);
+        goIv = findViewById(R.id.goIv);
 
-       /* Scanner scanner = new Scanner(System.in);
         int randomInt = new Random().nextInt(1000);
 
         int userInput = -1;
-        int numberOаAttempts = 0;
 
-        while (userInput != randomInt) {
-            userInput = scanner.nextInt();
-            numberOаAttempts++;
-            if (userInput < randomInt) {
-                System.out.println("Больше");
-            } else if (userInput > randomInt) {
-                System.out.println("Меньше");
+        goIv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int randomInt = new Random().nextInt(1000);
+
+                while (userInput != randomInt) {
+                    double userInput = Double.parseDouble(enterEt.getText().toString());
+                    if (userInput < randomInt) {
+                        endTv.setText("Больше");
+                    } else if (userInput > randomInt) {
+                        endTv.setText("Меньше");
+                    }
+                }
+                endTv.setText("Верно!!!");
             }
-        }
-        System.out.println("Количество попыток: " + numberOаAttempts);
-        System.out.println("Верно!!!");*/
+        });
+
     }
 }

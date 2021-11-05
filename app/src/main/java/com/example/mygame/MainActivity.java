3342c252afc4388ca495c18a1025b815b85ee744
Alehandro69 <work.personal.filippov@gmail.com>
2021-11-05 21:42:34 +0300

    new version 3

--- app/src/main/java/com/example/mygame/MainActivity.java
+++ app/src/main/java/com/example/mygame/MainActivity.java
@@ -4,12 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.OvershootInterpolator;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button gameBt;
+
+    FloatingActionButton fabMain, fabOne, fabTwo, fabThree;
+    Float translationY = 100f;
+
+    OvershootInterpolator interpolator = new OvershootInterpolator();
+
+    private static final String TAG = "MainActivity";
+
+    Boolean isMenuOpen = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +32,24 @@ public class MainActivity extends AppCompatActivity {
 
         gameBt = findViewById(R.id.gameBt);
 
+        fabMain = findViewById(R.id.fabMain);
+        fabOne = findViewById(R.id.fabOne);
+        fabTwo = findViewById(R.id.fabTwo);
+        fabThree = findViewById(R.id.fabThree);
+
+        fabOne.setAlpha(0f);
+        fabTwo.setAlpha(0f);
+        fabThree.setAlpha(0f);
+
+        fabOne.setTranslationY(translationY);
+        fabTwo.setTranslationY(translationY);
+        fabThree.setTranslationY(translationY);
+
+        fabMain.setOnClickListener(this);
+        fabOne.setOnClickListener(this);
+        fabTwo.setOnClickListener(this);
+        fabThree.setOnClickListener(this);
+
         gameBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,5 +57,63 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.fabMain:
+                Log.i(TAG, "onClick: fab main");
+                if (isMenuOpen) {
+                    closeMenu();
+                } else {
+                    openMenu();
+                }
+                break;
+            case R.id.fabOne:
+                Log.i(TAG, "onClick: fab one");
+                handleFabOne();
+                if (isMenuOpen) {
+                    closeMenu();
+                } else {
+                    openMenu();
+                }
+                break;
+            case R.id.fabTwo:
+                Log.i(TAG, "onClick: fab two");
+                break;
+            case R.id.fabThree:
+                Log.i(TAG, "onClick: fab three");
+                break;
+        }
+
+    }
+
+    private void openMenu() {
+        isMenuOpen = !isMenuOpen;
+
+        fabMain.animate().setInterpolator(interpolator).rotation(45f).setDuration(300).start();
+
+        fabOne.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start();
+        fabTwo.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start();
+        fabThree.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start();
+
+
+    }
+
+    private void closeMenu() {
+        isMenuOpen = !isMenuOpen;
+
+        fabMain.animate().setInterpolator(interpolator).rotation(0f).setDuration(300).start();
+
+        fabOne.animate().translationY(translationY).alpha(0f).setInterpolator(interpolator).setDuration(300).start();
+        fabTwo.animate().translationY(translationY).alpha(0f).setInterpolator(interpolator).setDuration(300).start();
+        fabThree.animate().translationY(translationY).alpha(0f).setInterpolator(interpolator).setDuration(300).start();
+
+    }
+
+    private void handleFabOne() {
+        Log.i(TAG, "handleFabOne: ");
     }
 }

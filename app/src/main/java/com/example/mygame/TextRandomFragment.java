45975e8800290103d541d2cc608c78b687f84c07
Alehandro69 <work.personal.filippov@gmail.com>
2021-11-07 21:50:55 +0300

    new version 3.1

--- app/src/main/java/com/example/mygame/TextRandomFragment.java
+++ app/src/main/java/com/example/mygame/TextRandomFragment.java
@@ -7,58 +7,43 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TextRandomFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class TextRandomFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TextRandomFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TextRandomFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TextRandomFragment newInstance(String param1, String param2) {
-        TextRandomFragment fragment = new TextRandomFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private String text1, text2, text3;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_text_random, container, false);
+        View v = inflater.inflate(R.layout.fragment_text_random, null);
+
+        TextView resulTv = (TextView) v.findViewById(R.id.resulTv);
+        EditText textEt1 = (EditText) v.findViewById(R.id.textEt1);
+        EditText textEt2 = (EditText) v.findViewById(R.id.textEt2);
+        EditText textEt3 = (EditText) v.findViewById(R.id.textEt3);
+        Button button = (Button) v.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text1 = textEt1.getText().toString().trim();
+                text2 = textEt2.getText().toString().trim();
+                text3 = textEt3.getText().toString().trim();
+
+                String[] myString = new String[]{text1, text2, text3};
+                int n = (int)Math.floor(Math.random() * myString.length);
+                resulTv.setText(myString[n]);
+            }
+        });
+
+
+
+        return v;
     }
 }
